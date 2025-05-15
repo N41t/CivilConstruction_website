0c6a370f93f263f18e5189884b8628e231960221
Michael Belozerov <belozerov.mg@gmail.com>
2025-05-15 15:08:08 +0700

    continuing to fill the Home Page

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -94,17 +94,22 @@ const Home = () => {
 
       </main>
 
+
+      {/* Footer Section */}
       <footer>
-        <div className='container'>
+        <div className='container py-5'>
           <div className='row'>
             <div className='col-md-3'>
               <h3>UrbanEdge Constructions</h3>
-              <p>Our post-construction services gives you peace of mind knowing that
+              <div className='pe-5'>
+                <p>Our post-construction services gives you peace of mind knowing that
                 we are still here for you even after.</p>
+              </div>
+              
             </div>
 
             <div className='col-md-3'>
-              <h3>Our Services</h3>
+              <h3 className='mb-3'>Our Services</h3>
               <ul>
                 <li>
                   <a href=''>Specialty Construction</a>
@@ -118,30 +123,55 @@ const Home = () => {
                 <li>
                   <a href=''>Corporate Construction</a>
                 </li>
+                <li>
+                  <a href=''>Building Constructions</a>
+                </li>
+                <li>
+                  <a href=''>Industrial Construction</a>
+                </li>
               </ul>
             </div>
 
             <div className='col-md-3'>
-              <h3>Our Services</h3>
+              <h3 className='mb-3'>Quick Links</h3>
               <ul>
                 <li>
-                  <a href=''>Specialty Construction</a>
+                  <a href=''>About us</a>
                 </li>
                 <li>
-                  <a href=''>Specialty Construction</a>
+                  <a href=''>Services</a>
                 </li>
                 <li>
-                  <a href=''>Specialty Construction</a>
+                  <a href=''>Projects</a>
                 </li>
                 <li>
-                  <a href=''>Specialty Construction</a>
+                  <a href=''>Blog</a>
+                </li>
+                <li>
+                  <a href=''>Contact us</a>
                 </li>
               </ul>
             </div>
 
             <div className='col-md-3'>
-              
+              <h3 className='mb-3'>Contact Us</h3>
+              <ul>
+                <li>
+                  <a href=''>(800-000-0000)</a>
+                </li>
+                <li>
+                  <a href=''>info@example.com</a>
+                </li>
+                <p>
+                  14738, Frewsburg, NY
+                </p>
+              </ul>
             </div>
+
+            <hr/>
+
+            <div className='text-center pt-4'>Copyright © 2025 UrbanEdge Constructions. All Rights Reserved.</div>
+
           </div>
         </div>
       </footer>
