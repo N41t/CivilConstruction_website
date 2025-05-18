e37bb3089fa6a2f19c1aec83a1593dc6a2b4e0dc
Michael Belozerov <belozerov.mg@gmail.com>
2025-05-19 00:06:16 +0700

    added Why Choose Us section

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -3,7 +3,11 @@ import AboutImg from '../../assets/images/about-us.jpg';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 import ServiceImg from '../../assets/images/construction1.jpg';
-import ConstructionImg from '../../assets/images/construction2.jpg';
+import ConstructionImg from '../../assets/images/construction2.jpg'; 
+import Icon1 from '../../assets/images/icon-1.svg';
+import Icon2 from '../../assets/images/icon-2.svg';
+import Icon3 from '../../assets/images/icon-3.svg';
+
 
 
 const Home = () => {
@@ -63,7 +67,7 @@ const Home = () => {
             </div>
           </section>
 
-          {/* Our Services */}
+          {/* Our Services Section */}
           <section className='section-3 bg-light py-5'>
             <div className='container-fluid py-5'>
               <div className='section-header text-center'>
@@ -168,7 +172,98 @@ const Home = () => {
             </div>
           </section>
 
-          {/* Our Projects */}
+
+          {/* Why Choose Us Section */}
+          
+          <section className='section-4 py-5'>
+            <div className='container py-5'>
+
+              <div className='section-header text-center'>
+                  <span>Why Choose Us</span>
+                  <h2>Discover our wide variety of projects</h2>
+                  <p>Created in close partnership with our clients and collaborators, this
+                    approach merges industry expertise,<br/>
+                    decades of experience, innovation, and flexibility to consistently deliver excellence.
+                  </p>
+                </div>
+
+              {/* Cards */}
+              <div className='row pt-4'>
+
+                {/* Cutting-Edge Solutions card */}
+                <div className='col-md-4'>
+                  <div className='card shadow border-0 p-4'>
+
+                    <div className='card-icon'>
+                      <img src={Icon1} alt="" />
+                    </div>
+
+                    <div className='card-title mt-3'>
+                      <h3>Cutting-Edge Solutions</h3>
+                    </div>
+
+                    <p>Small actions create big impacts. It all 
+                        begins and ends with each employee committing to
+                        safer work practices daily, ensuring they return 
+                        home safely.
+                    </p>
+                    
+
+                  </div>
+                </div>
+
+                {/* Superior Craftsmanship card */}
+                <div className='col-md-4'>
+                  <div className='card shadow border-0 p-4'>
+
+                    <div className='card-icon'>
+                      <img src={Icon2} alt="" />
+                    </div>
+
+                    <div className='card-title mt-3'>
+                      <h3>Superior Craftsmanship</h3>
+                    </div>
+
+                    <p>Small actions create big impacts. It all 
+                        begins and ends with each employee committing to
+                        safer work practices daily, ensuring they return 
+                        home safely.
+                    </p>
+                    
+
+                  </div>
+                </div>
+
+                {/* Knowledge and Expertise card */}
+                <div className='col-md-4'>
+                  <div className='card shadow border-0 p-4'>
+
+                    <div className='card-icon'>
+                      <img src={Icon3} alt="" />
+                    </div>
+
+                    <div className='card-title mt-3'>
+                      <h3>Knowledge and Expertise</h3>
+                    </div>
+
+                    <p>Small actions create big impacts. It all 
+                        begins and ends with each employee committing to
+                        safer work practices daily, ensuring they return 
+                        home safely.
+                    </p>
+                    
+
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+          </section>
+
+
+
+          {/* Our Projects Section */}
           <section className='section-3 bg-light py-5'>
             <div className='container-fluid py-5'>
               <div className='section-header text-center'>
