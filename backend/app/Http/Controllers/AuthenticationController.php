0e081d5ef64e08f6298a9e04890549416914edfc
Michael Belozerov <belozerov.mg@gmail.com>
2025-07-01 20:19:40 +0700

    added Authentication Controller and functionality configured

--- backend/app/Http/Controllers/AuthenticationController.php
+++ backend/app/Http/Controllers/AuthenticationController.php
@@ -3,10 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthenticationController extends Controller
 {
-    public function authenticate() {
-        echo "hello authenticate";
+    public function authenticate(Request $request) {
+        // Apply Validation 
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        } else {
+            $credentials = [
+                'email' => $request->email,
+                'password' => $request->password
+            ];
+
+            if (Auth::attempt($credentials)) {
+                return Auth::user();
+            } else {
+                return response()->json([
+                'status' => false,
+                'message' => 'Either email/password is incorrect.'
+                ]);
+            }
+        }
+
     }
 }
