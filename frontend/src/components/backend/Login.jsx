d503a26d02c4492ffeae72ea35832c891be60f7c
Michael Belozerov <belozerov.mg@gmail.com>
2025-07-07 21:26:51 +0700

    added validate Login form

--- frontend/src/components/backend/Login.jsx
+++ frontend/src/components/backend/Login.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import Header from '../common/Header';
 import Footer from '../common/Footer';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
   return (
     <>
         <Header/>
@@ -11,19 +24,43 @@ const Login = () => {
                     <div className='login-form my-5'>
                         <div className='card border-0 shadow'>
                             <div className='card-body p-4'>
-                                <form>
+                                <form onSubmit={handleSubmit(onSubmit)}>
                                     <h4 className='mb-3'>Login Here</h4>
                                     <div className='mb-3'>
                                         <label htmlFor='' className='form-label'>Email</label>
-                                        <input type='text' placeholder='Email' className='form-control'/>
+                                        <input 
+                                            {
+                                                ...register('email', {
+                                                    required: "This field is required",
+                                                    pattern: {
+                                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                                        message: "invalid email address"
+                                                    }
+                                                })
+                                            }
+                                        type='text' placeholder='Email' 
+                                        className={`form-control ${errors.email && 'is-invalid'}`}/>
+                                            {
+                                                errors.email && <p className='invalid-feedback'>{errors.email?.message}</p>
+                                            }
                                     </div>
 
                                     <div className='mb-3'>
                                         <label htmlFor='' className='form-label'>Password</label>
-                                        <input type='password' placeholder='Password' className='form-control'/>
+                                        <input 
+                                            {
+                                                ...register('password', {
+                                                    required: "This field is required"
+                                                })
+                                            }
+                                        type='password' placeholder='Password'
+                                        className={`form-control ${errors.password && 'is-invalid'}`}/>
+                                            {
+                                                errors.password && <p className='invalid-feedback'>{errors.password?.message}</p>
+                                            }
                                     </div>
 
-                                    <button className='btn btn-primary'>Login</button>
+                                    <button type='submit' className='btn btn-primary'>Login</button>
                                 </form>
                             </div>
                         </div>
