00fb969663b3b3e76c74ad2e3ab95134888f56f5
Michael Belozerov <belozerov.mg@gmail.com>
2025-07-17 22:19:47 +0700

    created context & provider (added logout)

--- frontend/src/components/backend/Login.jsx
+++ frontend/src/components/backend/Login.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 import { useForm } from "react-hook-form";
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from './context/Auth';
 
 const Login = () => {
+
+    const {login} = useContext(AuthContext);
 
     const navigate = useNavigate();
 
@@ -39,6 +42,8 @@ const Login = () => {
             }
 
             localStorage.setItem('userInfo', JSON.stringify(userInfo))
+
+            login(userInfo);
 
             navigate('/admin/dashboard')
         }
