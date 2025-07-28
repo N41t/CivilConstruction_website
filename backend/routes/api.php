fa201f8ad33eaa9d84119599e15ff86805ff0f0e
Michael Belozerov <belozerov.mg@gmail.com>
2025-07-28 23:30:53 +0700

    improved ServiceController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\DashboardController;
+use App\Http\Controllers\admin\ServiceController;
 use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,8 +17,14 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
     
     // Protected Routes
     Route::get('/dashboard', [DashboardController::class, 'index']);
-
     Route::get('/logout', [AuthenticationController::class, 'logout']);
+
+
+    // Service Routes
+    Route::post('/services', [ServiceController::class, 'store']);
+    Route::get('/services', [ServiceController::class, 'index']);
+
+
 });
 
 
